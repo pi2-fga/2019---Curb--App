09e9fd4454aa7425bf60678a729f5e021d77f285
gustavo2795 <gustavo.unb27@gmail.com>
2019-05-16 03:25:22 -0300

    Creating qrcode scan flux

--- Curb/src/Routes.js
+++ Curb/src/Routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Home from './components/Home';
 import { Router, Scene, root } from 'react-native-router-flux';
+import ScanScreen from './components/ScanScreen';
+import Dashboard from './components/Dashboard';
 
 const Rotas = () => (
-    <Router sceneStyle={{ paddingTop: 25}}>
+    <Router sceneStyle={{ paddingTop: 24}}>
         <Scene key='root'>
-            <Scene key='Home' component={Home} initial title='Home'/>
+            <Scene key='Home' component={Home} initial hideNavBar={true} title='Home'/>
+            <Scene key='ScanScreen' component={ScanScreen} hideNavBar={true} title='Scan'/>
+            <Scene key='Dashboard' component={Dashboard} hideNavBar={true} title='Dashboard'/>
         </Scene>
     </Router>
 );
--- Curb/src/components/Home.js
+++ Curb/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Dimensions, StatusBar, ImageBackground } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 var screenWidth = Dimensions.get('window').width;
 
@@ -7,14 +8,18 @@ export default class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image
-            style={styles.logo}
-            source={require('../../assets/img/logo.png')}
-        />
-        <Button style={styles.button}
-            title = 'Adicionar Curb'
-            color = '#F1C40F'
-        />
+            <StatusBar ></StatusBar>
+            <ImageBackground source={require('../../assets/img/FotoJet.png')} style={styles.imageBackground}>
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/img/logo.png')}
+                />
+                <Button style={styles.button}
+                    title = 'Adicionar Curb'
+                    color = '#e2c044'
+                    onPress={() => { Actions.ScanScreen(); }}
+                />
+            </ImageBackground>
       </View>
     );
   }
@@ -23,17 +28,22 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#95A5A6',
         justifyContent: 'center',
         alignItems: 'center'
     },
     button: {
         width: screenWidth * 0.9,
-        height: 30
+        height: 60
     },
     logo: {
         width: screenWidth * 0.9, 
         height: screenWidth * 0.6,
         marginTop: 40,
+        marginLeft: 18,
+        marginBottom: 120
+    },
+    imageBackground: {
+        width: '100%', 
+        height: '100%'
     }
 })
